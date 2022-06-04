1206f43af104d724b5f4bbf620c527cbcd2e57d3
Shehab <shehab.m.sadek@gmail.com>
2022-06-04 23:45:16 +0200

    Update DsPQ.cpp
    Optimized outputs, bug fixes

--- DsPQ.cpp
+++ DsPQ.cpp
@@ -14,7 +14,7 @@ bool isPal(string s, int l, int h) {
     return true;
 }
 
-void PalUtil(int ind, string s, vector<string>& curParition, vector<vector<string>> &result) {
+void PalUtil(int ind, string s, vector<string>& curParition, vector<vector<string>>& result) {
     if (ind == s.size()) {
         result.push_back(curParition);
         return;
@@ -32,7 +32,7 @@ void PalUtil(int ind, string s, vector<string>& curParition, vector<vector<strin
 void printMinNumberOfCuts(vector<vector<string>> result) {
     vector<string> minPartition;
     int minSize = INT_MAX;
-    for(auto i : result){ 
+    for (auto i : result) {
         if (i.size() < minSize) {
             minPartition.clear();
             for (auto j : i) {
@@ -41,45 +41,51 @@ void printMinNumberOfCuts(vector<vector<string>> result) {
         }
     }
 
-    
+    int ctr = 31;
+    for (auto i : minPartition) {
+        //cout << i << " ";
+        if (ctr == 37)
+            ctr = 91;
+        cout <<"\033["<<ctr<<"m"<<i<<"\033[0m|";
+        ctr++;
+    }
 
-    cout << "min Number Of Cuts(recursive) : " << minPartition.size() - 1 << endl;
-
-    for (auto i : minPartition)
-        cout << i << " ";
+    cout << "\n\n Minimum Number Of Cuts\033[91m(recursive) :\033[0m" << minPartition.size() - 1 << endl;
 }
 
 vector<vector<string>> partition(string s) {
     vector<vector<string>> result;
     vector<string> curPartition;
     PalUtil(0, s, curPartition, result);
+    
     //for (auto i : result) {
     //    for (auto j : i) {
     //        cout << j << " ";
     //    }
     //    cout << endl;
     //}
-
+    
     printMinNumberOfCuts(result);
     return result;
 
 }
 
 //-------------------------
-int palindromePartion(string str) {
+int palindromePartition(string str) {
     int length = str.length();
     int* cut = new int[length];
-
     bool** isPalindrome = new bool* [length];
+
     for (int i = 0; i < length; i++) {
         isPalindrome[i] = new bool[length];    //Dynamically create 2d boolean array with intial values of "False"
         for (int j = 0; j < length; j++) {
             isPalindrome[i][j] = false;
         }
     }
+
     /*for (int i = 0; i < length; i++)
     {
-        isPalindrome[i][i] = true; //Set the diagonal of the matrix "True" single letter case palindrome [done on line 29 now]
+        isPalindrome[i][i] = true; //Set the diagonal of the matrix "True" single letter case palindrome [done on line 93 now]
     }*/
 
     for (int i = 0; i < length; i++)
@@ -98,12 +104,17 @@ int palindromePartion(string str) {
     }
     return cut[length - 1];
 }
+void displayOutput() {
+    
+}
 
 int main()
 {
-    string s = "aabb";
+    string s = "ababxyutab";
+    cout << " String input :"<<s<<endl;
+    cout << "\n String Partitioning :";
     partition(s);
     cout << endl;
-    cout << "Minimum number of cuts(dynamic) : " << palindromePartion(s);
+    cout << " Minimum number of cuts\033[93m(dynamic) :\033[0m" << palindromePartition(s)<<endl;
 }
 
